ece3c422a5c55764573d117d5d748a95868ca46a
Phil Fehlinger <pjfehlinger@gmail.com>
2024-04-01 21:40:16 -0400

    Added documentation for category.py

--- src/risk_adjustment_model/category.py
+++ src/risk_adjustment_model/category.py
@@ -1,39 +1,105 @@
+from typing import Type, Union, List
+from .reference_files_loader import ReferenceFilesLoader
+
+
 class Category:
     """
-    This is an object to encapsulate a single category. It will contain
-    category metadata along with the "weight" of the category
-    for the appropriate model population
+    Encapsulates metadata for a single category.
+
+    This class represents a category with its metadata and the coefficient assigned to it
+    for a specific model population.
+
+    Attributes:
+        reference_files: An instantiated ReferenceFilesLoader class containing category definitions and coefficients.
+        risk_model_population (str): The population type of the beneficiary used for scoring.
+        category (str): The name of the category, e.g. "HCC1", "F0_34".
+        mapper_codes (list, optional): Mapping codes associated with the category (default is None).
+        dropped_categories (list, optional): List of dropped categories (default is None).
+        type (str):
+        description (str):
+        coefficient (float):
+        number (int):
+
     """
 
     def __init__(
         self,
-        reference_files,
-        risk_model_population,
-        category,
-        mapper_codes=None,
-        dropped_categories=None,
+        reference_files: Type[ReferenceFilesLoader],
+        risk_model_population: str,
+        category: str,
+        mapper_codes: Union[None, List[str]] = None,
+        dropped_categories: Union[None, List[str]] = None,
     ):
+        """
+        Initialize a Category object.
+
+        Args:
+            reference_files: An instantiated ReferenceFilesLoader class containing category definitions and coefficients.
+            risk_model_population (str): The population type of the beneficiary used for scoring.
+            category (str): The name of the category, e.g. "HCC1", "F0_34".
+            mapper_codes (list, optional): Mapping codes associated with the category (default is None).
+            dropped_categories (list, optional): List of dropped categories (default is None)..
+
+        """
         self.reference_files = reference_files
         self.risk_model_population = risk_model_population
         self.category = category
+        self.mapper_codes = mapper_codes
+        self.dropped_categories = dropped_categories
         self.type = self._get_type(category)
         self.description = self._get_description(category)
         self.coefficient = self._get_coefficient(category, risk_model_population)
         self.number = self._get_number(category)
-        self.mapper_codes = mapper_codes
-        self.dropped_categories = dropped_categories
 
-    def _get_type(self, category):
+    def _get_type(self, category: str):
+        """
+        Retrieve the type of the category from the reference files.
+
+        Args:
+            category (str): The name of the category.
+
+        Returns:
+            str: The type of the category.
+
+        """
         return self.reference_files.category_definitions[category]["type"]
 
-    def _get_description(self, category):
+    def _get_description(self, category: str):
+        """
+        Retrieve the description of the category from the reference files.
+
+        Args:
+            category (str): The name of the category.
+
+        Returns:
+            str: The description of the category.
+
+        """
         return self.reference_files.category_definitions[category]["descr"]
 
-    def _get_coefficient(self, category, risk_model_population):
+    def _get_coefficient(self, category: str, risk_model_population: str):
+        """
+        Retrieve the coefficient of the category for the given population from the reference files.
+
+        Args:
+            category (str): The name of the category.
+            risk_model_population (str): The population type for the risk model.
+
+        Returns:
+            float: The coefficient of the category for the given population.
+
+        """
         return self.reference_files.category_weights[category][risk_model_population]
 
-    def _get_number(self, category):
-        return self.reference_files.category_definitions[category].get("number", None)
+    def _get_number(self, category: str):
+        """
+        Retrieve the number associated with the category from the reference files.
 
-    def _get_number(self, category):
+        Args:
+            category (str): The name of the category.
+
+        Returns:
+            int or None: The number associated with the category, or None if not found.
+
+        """
         return self.reference_files.category_definitions[category].get("number", None)
