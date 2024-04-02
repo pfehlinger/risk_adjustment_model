88cde2c35bf9090379435069fbaf7f462dd8c323
Phil Fehlinger <pjfehlinger@gmail.com>
2024-04-02 10:38:03 -0400

    Fixing duplicate values in scoring result class

--- src/risk_adjustment_model/result.py
+++ src/risk_adjustment_model/result.py
@@ -59,25 +59,3 @@ class ScoringResult:
     demographic_score: float
     category_list: List[str]
     category_details: Dict[str, str]
-    gender: str
-    orec: str
-    medicaid: bool
-    age: int
-    dob: str
-    diagnosis_codes: list
-    year: int
-    population: str
-    risk_model_age: int
-    risk_model_population: str
-    model_version: str
-    model_year: int
-    coding_intensity_adjuster: float
-    normalization_factor: float
-    score_raw: float
-    disease_score_raw: float
-    demographic_score_raw: float
-    score: float
-    disease_score: float
-    demographic_score: float
-    category_list: list
-    category_details: dict
