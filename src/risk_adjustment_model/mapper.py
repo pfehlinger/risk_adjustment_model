0e605269f0b7f34a0fd75a7d2c7d905d55f27c27
Phil Fehlinger <pjfehlinger@gmail.com>
2024-04-02 09:09:15 -0400

    Documentation for category.py

--- src/risk_adjustment_model/mapper.py
+++ src/risk_adjustment_model/mapper.py
@@ -1,9 +1,27 @@
+from typing import Union
+
+
 class GenericCodeCategory:
     """
-    This class is to encapsulate a Generic code and what category it maps to
+    Encapsulates a generic code and its corresponding category mapping. This is a base
+    class and should not be called directly.
+
+    Attributes:
+        category_map (dict): A dictionary containing the mapping of codes to categories.
+        mapper_code (str): The code to be mapped.
+        type (str): The type of code (default is None).
+        category (list): A list containing the category corresponding to the code.
     """
 
-    def __init__(self, category_map, code, type=None):
+    def __init__(self, category_map: dict, code: str, type: Union[str, None] = None):
+        """
+        Initializes GenericCodeCategory with the provided parameters.
+
+        Args:
+            category_map (dict): A dictionary containing the mapping of codes to categories.
+            code (str): The code to be mapped.
+            type (str, optional): The type of code (default is None).
+        """
         self.mapper_code = code
         self.type = type
         self.category_map = category_map[type]
@@ -11,15 +29,66 @@ class GenericCodeCategory:
 
 
 class DxCodeCategory(GenericCodeCategory):
-    def __init__(self, filepath, code, type="diag"):
-        super().__init__(filepath, code, type)
+    """
+    Represents a diagnosis code and its category mapping.
+
+    Attributes:
+        category_map (dict): A dictionary containing the mapping of codes to categories.
+        code (str): The diagnosis code.
+        type (str): The type of code, defaulted to "diag".
+    """
+
+    def __init__(self, category_map: dict, code: str, type: str = "diag"):
+        """
+        Initializes DxCodeCategory with the provided parameters.
+
+        Args:
+            category_map (dict): A dictionary containing the mapping of codes to categories.
+            code (str): The diagnosis code.
+            type (str, optional): The type of code (default is "diag").
+        """
+        super().__init__(category_map, code, type)
 
 
 class NDCCodeCategory(GenericCodeCategory):
-    def __init__(self, filepath, code, type="ndc"):
-        super().__init__(filepath, code, type)
+    """
+    Represents a National Drug Code (NDC) and its category mapping.
+
+    Attributes:
+        category_map (dict): A dictionary containing the mapping of codes to categories.
+        code (str): The NDC code.
+        type (str): The type of code, defaulted to "ndc".
+    """
+
+    def __init__(self, category_map: dict, code: str, type: str = "ndc"):
+        """
+        Initializes NDCCodeCategory with the provided parameters.
+
+        Args:
+            category_map (dict): A dictionary containing the mapping of codes to categories.
+            code (str): The NDC code.
+            type (str, optional): The type of code (default is "ndc").
+        """
+        super().__init__(category_map, code, type)
 
 
 class ProcCodeCategory(GenericCodeCategory):
-    def __init__(self, filepath, code, type="proc"):
-        super().__init__(filepath, code, type)
+    """
+    Represents a procedure code (CPT, HCPCS, ICD10-CM codes, etc.) and its category mapping.
+
+    Attributes:
+        category_map (dict): A dictionary containing the mapping of codes to categories.
+        code (str): The procedure code.
+        type (str): The type of code, defaulted to "proc".
+    """
+
+    def __init__(self, category_map: dict, code: str, type: str = "proc"):
+        """
+        Initializes ProcCodeCategory with the provided parameters.
+
+        Args:
+            category_map (dict): A dictionary containing the mapping of codes to categories.
+            code (str): The procedure code.
+            type (str, optional): The type of code (default is "proc").
+        """
+        super().__init__(category_map, code, type)
